385732989e0ead62d6cafc9b926ba0a74336deb0
TianDong <276448618@qq.com>
2022-10-28 15:32:25 +0800

    first vue3

---
+++ src/views/login/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="login">
+    <h1>{{msg}}</h1>
+    <el-button @click="defclick">Default</el-button>
+  </div>
+</template>
+<script>
+  export default {
+    data(){
+      return {
+        msg: '田东'
+      }
+    },
+    methods: {
+      defclick(){
+        this.msg = 'Hello'
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  #login{
+    height: 100vh;
+    background: #fe0202;
+  }
+</style>
